82b397e4c6ce4667e17a0e28d055307281f8f4c6
Peter Stromquist <peter@twistedstream.com>
2016-08-22 15:51:58 -0700

    Fix 500 2

--- webtask.js
+++ webtask.js
@@ -4,10 +4,10 @@ var Webtask = require('webtask-tools');
 // This is the entry-point for the Webpack build. We need to convert our module
 // (which is a simple Express server) into a Webtask-compatible function.
 module.exports = Webtask.fromExpress((req, res) => {
-  nconf
-    .defaults({
-      AUTH0_APIV2_TOKEN: req.webtaskContext.secrets.AUTH0_APIV2_TOKEN
-    });
+  // nconf
+  //   .defaults({
+  //     AUTH0_APIV2_TOKEN: req.webtaskContext.secrets.AUTH0_APIV2_TOKEN
+  //   });
 
   // Start the server.
   const app = require('./index');
